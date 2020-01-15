5ffe08fc3364104882f4181c60b4f1c6510e97b6
l1z4rd8 <56437411+l1z4rd8@users.noreply.github.com>
2020-01-15 18:56:11 +0300

    Create app.py

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, import status
+app = Flask(__name__)
+@app.route('/')
+def home():
+   return {'welcome': 'to': 'my': 'app'}
+
+@app.route('/status/')
+def empty_view(self):
+    content = {''}
+    return content, status.HTTP_204_NO_CONTENT
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
