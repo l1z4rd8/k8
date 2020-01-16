02908512b060bd115605d1b6d94cf77a82211f63
l1z4rd8 <56437411+l1z4rd8@users.noreply.github.com>
2020-01-16 13:24:37 +0300

    Update app.py

--- app.py
+++ app.py
@@ -3,13 +3,10 @@ from flask_api import status
 app = Flask(__name__)
 @app.route('/')
 def home():
-   return "<h1>Welcome to my app</h1>", 200, {"X-Special": "Value"}
-
+    return "<h1>Welcome to my app</h1>", 200, {"X-Special": "Value"}
 @app.route('/status/')
 def empty_view(self):
     content = {''}
     return content, status.HTTP_204_NO_CONTENT
 if __name__ == '__main__':
     app.run(debug=True)
-
-
